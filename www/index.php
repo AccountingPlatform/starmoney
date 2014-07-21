5604ab87cc81b2e9802ca755065ad16f5e1f1eef
refine1017 <972842322@qq.com>
2014-07-21 22:59:22 +0800

    修改了index.php

--- www/index.php
+++ www/index.php
@@ -6,13 +6,11 @@
  * Time: 下午10:21
  */
 
-//  定义网站根目录
+// 定义网站根目录
 define('WEB_ROOT', dir(dir(__FILE__)));
-
-//  
-
+// 获取数据库配置文件
 $dbconf = include(WEB_ROOT . "/etc/dbconfig.php");
-
+// 连接数据库，选择数据库，设置字符集
 $link = mysql_connect($dbconf['dbhost'], $dbconf['dbuser'], $dbconf['dbpwd']) or die('connect mysql error!' . mysql_error());
 mysql_select_db($dbconf['dbname'], $link) or die('select db error!' . mysql_error());
 $sql = "set names '" . $dbconf['dbname'] . "'";
