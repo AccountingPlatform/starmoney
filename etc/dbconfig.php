520e4dab0911c1cb872a6bfef9ddbd4d91998f12
refine1017 <972842322@qq.com>
2014-07-21 22:53:19 +0800

    创建数据库配置文件

---
+++ etc/dbconfig.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: zhangxing
+ * Date: 14-7-21
+ * Time: 下午10:31
+ */
+return array(
+    "dbhost" => "localhost",
+    "dbname" => "starmoney",
+    "dbuser" => "root",
+    "dbpwd"  => "root",
+    "charset" => "utf-8"
+);
--- www/index.php
+++ www/index.php
@@ -4,4 +4,15 @@
  * User: zhangxing
  * Date: 14-7-21
  * Time: 下午10:21
- */ 
+ */
+
+//  定义网站根目录
+define('WEB_ROOT', dir(dir(__FILE__)));
+
+//  
+
+$dbconf = include(WEB_ROOT . "/etc/dbconfig.php");
+
+$link = mysql_connect($dbconf['dbhost'], $dbconf['dbuser'], $dbconf['dbpwd']) or die('connect mysql error!' . mysql_error());
+mysql_select_db($dbconf['dbname'], $link) or die('select db error!' . mysql_error());
+$sql = "set names '" . $dbconf['dbname'] . "'";
